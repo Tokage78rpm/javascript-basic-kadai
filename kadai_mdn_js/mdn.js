1a0353c5dc3cea28f6b8e930532502fff7a6af7d
Tokage78rpm <66306563+Tokage78rpm@users.noreply.github.com>
2023-11-14 11:07:31 +0900

    Update mdn.js

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -4,8 +4,19 @@ console.log(date);
 
 
 const [month, day, year] = [
-  date.getMonth(),
+  date.getMonth()+1,
   date.getDate(),
   date.getFullYear(),
 ];
 console.log(year+'年'+month+'月'+day+'日');
+
+/*　もしくは
+
+const [month, day, year] = [
+  date.getMonth(),
+  date.getDate(),
+  date.getFullYear(),
+];
+console.log(year+'年'+(month+1)+'月'+day+'日');
+
+*/
